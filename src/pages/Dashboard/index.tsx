7c75e159b5b888471ce60fe40fadf9ec947821aa
Lucas Cunha <lucasviana112@gmail.com>
2020-07-21 14:26:50 -0300

    initial structure for dashboard page

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Dashboard: React.FC = ({ children }) => {
+  return (
+    <div>
+      {children}
+      <h1>dashboard</h1>
+      <h1>dashboard</h1>
+      <h1>dashboard</h1>
+      <h1>dashboard</h1>
+      <h1>dashboard</h1>
+      <h1>dashboard</h1>
+    </div>
+  );
+};
+
+export default Dashboard;
